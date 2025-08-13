aaab4e9f1d1dc3dd2518342c2559eebf328212f8
Akshansh Khare <akshanskhare@gmail.com>
2025-08-13 11:59:09 +0530

    try to make carsoule responsive in realme 12x (didn't succeed)

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -4,12 +4,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function Portfolio() {
+
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3, // desktop
+        slidesToShow: 3,
         slidesToScroll: 1,
+
         responsive: [
             {
                 breakpoint: 1024, // <= 1024px
